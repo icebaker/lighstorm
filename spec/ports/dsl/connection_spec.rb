38a6b668f00a08325fc3834c37cc2a3e2e178815
icebaker <icebaker@proton.me>
2023-03-19 15:10:58 -0300

    fixing vcr

--- spec/ports/dsl/connection_spec.rb
+++ spec/ports/dsl/connection_spec.rb
@@ -15,14 +15,19 @@ RSpec.describe Lighstorm::Connection do
       'lndconnect://127.0.0.1:10002?cert=MIICHjCCAcOgAwIBAgIRAKFIzgCaW7Ad8EZ_TNpnQhAwCgYIKoZIzj0EAwIwLzEfMB0GA1UEChMWbG5kIGF1dG9nZW5lcmF0ZWQgY2VydDEMMAoGA1UEAxMDYm9iMB4XDTIzMDMxMjIzNDQxMVoXDTI0MDUwNjIzNDQxMVowLzEfMB0GA1UEChMWbG5kIGF1dG9nZW5lcmF0ZWQgY2VydDEMMAoGA1UEAxMDYm9iMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEBrwjgH47U5oPJ0XF2yn35MLSJw3dt7kRufh4jmlkETiV-5hhDQ-pDrSjkMUaK8Bj4AuZFW5h0NPbHqSX-MjONKOBvzCBvDAOBgNVHQ8BAf8EBAMCAqQwEwYDVR0lBAwwCgYIKwYBBQUHAwEwDwYDVR0TAQH_BAUwAwEB_zAdBgNVHQ4EFgQUfam6_RZHlYm3Xhj07E53WyLcIAcwZQYDVR0RBF4wXIIDYm9igglsb2NhbGhvc3SCA2JvYoIMcG9sYXItbjEtYm9iggR1bml4ggp1bml4cGFja2V0ggdidWZjb25uhwR_AAABhxAAAAAAAAAAAAAAAAAAAAABhwSsGAAFMAoGCCqGSM49BAMCA0kAMEYCIQDMewc33jp0SlMW6xbUJKEPJRUg5tdNg7A8wPrT0fo2_gIhAP1gQFzNV9ffFD7I_y4Pyat49rPqTNBFttLbFUSbUgYw&macaroon=AgEDbG5kAvgBAwoQNPLBvSsr2kM1p0M1OiPRxhIBMBoWCgdhZGRyZXNzEgRyZWFkEgV3cml0ZRoTCgRpbmZvEgRyZWFkEgV3cml0ZRoXCghpbnZvaWNlcxIEcmVhZBIFd3JpdGUaIQoIbWFjYXJvb24SCGdlbmVyYXRlEgRyZWFkEgV3cml0ZRoWCgdtZXNzYWdlEgRyZWFkEgV3cml0ZRoXCghvZmZjaGFpbhIEcmVhZBIFd3JpdGUaFgoHb25jaGFpbhIEcmVhZBIFd3JpdGUaFAoFcGVlcnMSBHJlYWQSBXdyaXRlGhgKBnNpZ25lchIIZ2VuZXJhdGUSBHJlYWQAAAYg79y4thPvtaZ388GF664C1lfRcxPwcZR7HdKzh1ADw48'
     )
 
-    expect(described_class.default.keys.sort).to include(
+    expect(
+      VCR.tape.replay('Lighstorm::Connection.default.keys') do
+        described_class.default.keys.sort
+      end
+    ).to include(
       :address, :certificate, :macaroon, :certificate_path, :macaroon_path
     )
 
     expect(
       VCR.tape.replay('Lighstorm::Connection.default.address') do
         described_class.default[:address]
-      end).to eq(
+      end
+    ).to eq(
       '127.0.0.1:10009'
     )
 
